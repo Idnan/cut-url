0002fc9a04e3e887c2ec0e4db094090717f8faff
Adnan Ahmed <mahradnan@hotmail.com>
2016-03-30 00:59:32 +0400

    lets begin

---
+++ assets/js/background.js
@@ -0,0 +1,46 @@
+// Chrome event handler
+chrome.commands.onCommand.addListener(function (command) {
+
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+
+    	if (tabs[0] && tabs[0].url) {
+    		var longUrl = tabs[0].url;
+	        var json = {'longUrl': longUrl};
+
+	        $.ajax({
+	            'url': 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyBbm-qp6l_JIe-rbmBX1ebN2xWmIWtEpII',
+	            headers: {
+	                'Content-Type': 'application/json'
+	            },
+	            'method': 'POST',
+	            'dataType': 'json',
+	            'data': JSON.stringify(json),
+	            success: function (response) {
+
+	                if (response) {
+	                    shortUrl = response.id;
+	                    copyToClipboard(shortUrl);
+	                    show(shortUrl);
+	                }
+	            }
+	        });
+    	}
+    });
+});
+
+function copyToClipboard(shortUrl) {
+    var input = document.createElement('textarea');
+    document.body.appendChild(input);
+    input.value = shortUrl;
+    input.focus();
+    input.select();
+    document.execCommand('Copy');
+    input.remove();
+}
+
+function show(shortUrl) {
+    new Notification('Url Shortened', {
+        icon: '../img/ico48.png',
+        body: shortUrl + ' (Copied to clipboard)'
+    });
+}
